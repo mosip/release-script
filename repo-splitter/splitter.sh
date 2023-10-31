9ad936cecd3a76c86b0b1db26dd418b743c320a9
Mahesh-Binayak <76687012+Mahesh-Binayak@users.noreply.github.com>
2023-10-31 09:54:04 +0530

    [MOSIP-30003]Update splitter.sh
    Signed-off-by: Mahesh-Binayak <76687012+Mahesh-Binayak@users.noreply.github.com>

--- repo-splitter/splitter.sh
+++ repo-splitter/splitter.sh
@@ -3,20 +3,34 @@ exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec > >(tee -a log.out) 2>&1
 
-# Define repository URLs
-EXISTING_REPO_URL="https://github.com/ckm007/commons.git"
-NEW_REPO_URL="https://github.com/ckm007/otpmanager-new.git"
+# Load properties from repo.properties
+if [ -f "repo.properties" ]; then
+    source "repo.properties"
+else
+    echo "Error: repo.properties file not found."
+    exit 1
+fi
 
-# Specify the list of files and folders to preserve
-FILES_TO_MOVE=(
-  ".github"
-  "db_scripts"
-  ".gitignore"
-  "pom.xml"
-  "README.md"
-  "LICENSE"
-  "kernel/kernel-otpmanager-service"
-)
+# Extract the repository name from the existing repository URL
+REPO_NAME=$(basename "$EXISTING_REPO_URL" .git)
+
+# Check if the directory with the name REPO_NAME exists
+if [ -d "$REPO_NAME" ]; then
+    read -p $'\e[91mSource Repo already seems to be available at present location, Do you want to override? (y/n): \e[0m' ANSWER
+    if [[ "$ANSWER" == "y" ]]; then
+        # Delete current repo and re-clone
+        rm -rf "$REPO_NAME"
+        git clone "$EXISTING_REPO_URL"
+    elif [[ "$ANSWER" == "n" ]]; then
+        echo "Proceeding with the existing directory."
+    else
+        echo "Invalid choice. Exiting."
+        exit 1
+    fi
+else
+    # Directory doesn't exist, clone the repository
+    git clone "$EXISTING_REPO_URL"
+fi
 
 # Check if python3 is available
 if command -v python3 &> /dev/null; then
@@ -33,26 +47,23 @@ else
     PYTHON_CMD="python3"
     PIP_CMD="pip3"
 fi
-
 # Install filter-repo
 echo "Install filter-repo using " $PIP_CMD
 $PIP_CMD install git-filter-repo
 
-# Extract the repository name from the existing repository URL
-
-REPO_NAME=$(basename "$EXISTING_REPO_URL" .git)
-
-echo "Cloning " "$REPO_NAME" 
-# Clone the existing repository and change to the repository directory
-git clone "$EXISTING_REPO_URL"
 cd "$REPO_NAME"
+
+# Convert FILES_TO_MOVE into an array
+IFS=' ' read -r -a FILES_TO_MOVE <<< "$FILES_TO_MOVE"
 
 # Create a list of paths to preserve based on the OR condition
 echo "setting up preserved path"
 PRESERVE_PATHS=()
 for item in "${FILES_TO_MOVE[@]}"; do
-  PRESERVE_PATHS+=("--path" "$item")
+  PRESERVE_PATHS+=("--path")
+  PRESERVE_PATHS+=("$item")
 done
+
 
 # Use git filter-repo to create a filtered repository
 echo "Filtering the repo for mentioned services" "${PRESERVE_PATHS[@]}"
